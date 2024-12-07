19b6d0c2f98b20299c5fea1d5883e809a051dc9b
root <root@DESKTOP-EJ8HNA8.localdomain>
2024-12-07 11:07:42 +0100

    Improve scraper performance

--- data.py
+++ data.py
@@ -1,32 +1,29 @@
 import asyncio
 import random
 import aiohttp
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import requests
 
 
 async def get_definition(word, session, i):
     url = f"https://dictionary.cambridge.org/dictionary/english/{word}"
     headers = {'user-agent': 'x-infinity/0.0.1'}
-    print(f"downloading {i}")
     async with session.get(url, headers=headers) as response:
-        print(f"downloaded {i}")
-        soup = BeautifulSoup(await response.text(), "html.parser")
-    definition = soup.find(attrs=["class", "def ddef_d db"])
+        divs = SoupStrainer(attrs=["class", "def ddef_d db"])
+        definition = BeautifulSoup(await response.text(), "lxml", parse_only=divs).div
     if not definition:
         return None
 
     definition = definition.text.strip()
     if definition[-1] == ":":
         definition = definition[:-1]
-    return "a"
+    return definition
 
 
 async def get_multiple_definitions(words):
     definitions = []
     async with aiohttp.ClientSession() as session:
         for i, word in enumerate(words):
-            print(word)
             definitions.append(get_definition(word, session, i))
         definitions = await asyncio.gather(*definitions)
     return definitions
@@ -39,8 +36,8 @@ def main():
     long_words = [word for word in words if len(word) >= 4]
     saved_words = random.sample(long_words, 100)
 
-    results = asyncio.run(get_multiple_definitions(saved_words))
-    results = [word for word in results if word]
+    definitions = asyncio.run(get_multiple_definitions(saved_words))
+    results = [f"{saved_words[i]},{definitions[i]}" for i in range(len(definitions)) if definitions[i]]
 
     with open("words.txt", "w") as file:
         file.write("\n".join(results))
