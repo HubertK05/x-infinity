0ffb7c8720e143d16a00fee49931df3dc9018db7
root <root@DESKTOP-EJ8HNA8.>
2024-12-27 13:09:33 +0100

    Add more test cases for random word matching

--- test_crossword_generator.py
+++ test_crossword_generator.py
@@ -1,6 +1,8 @@
+import pytest
 from crossword import Crossword
 from crossword_generator import CrosswordGenerator
 from entry import Entry
+from util import CrosswordGenerationError
 
 
 def test_crossword_generator_init():
@@ -15,13 +17,40 @@ def test_crossword_generator_set_entries():
     assert generator.entries == [entry]
 
 
-def test_generate_crossword():
+def noop(_collection):
+    pass
+
+
+def test_find_random_matching(monkeypatch):
+    monkeypatch.setattr("random.shuffle", noop)
+
     generator = CrosswordGenerator()
     the_entry = Entry("the", "the definition")
+    a_entry = Entry("a", "a definition")
     cat_entry = Entry("cat", "cat definition")
-    a_entry = Entry("a", "a definition"),
-    entries = [the_entry, cat_entry, a_entry]
+    entries = [the_entry, a_entry, cat_entry]
     generator.entries = entries
+
+    result: list[tuple[int, Entry]] = generator._find_random_matching("eat")
+
+    # This is the only solution
+    assert result == [
+        (2, the_entry),
+        (0, a_entry),
+        (2, cat_entry),
+    ]
+
+
+def test_generate_crossword_using_random_matching(monkeypatch):
+    monkeypatch.setattr("random.shuffle", noop)
+
+    generator = CrosswordGenerator()
+    the_entry = Entry("the", "the definition")
+    a_entry = Entry("a", "a definition")
+    cat_entry = Entry("cat", "cat definition")
+    entries = [the_entry, a_entry, cat_entry]
+    generator.entries = entries
+
     result: Crossword = generator.generate("eat")
 
     # This is the only solution
@@ -31,3 +60,15 @@ def test_generate_crossword():
         (2, a_entry),
         (0, cat_entry),
     ]
+
+
+def test_find_random_matching_fails_on_insufficient_words():
+    generator = CrosswordGenerator()
+    the_entry = Entry("the", "the definition")
+    a_entry = Entry("a", "a definition")
+    cat_entry = Entry("cat", "cat definition")
+    entries = [the_entry, a_entry, cat_entry]
+    generator.entries = entries
+
+    with pytest.raises(CrosswordGenerationError):
+        generator._find_random_matching("aca")
--- util.py
+++ util.py
@@ -1,3 +1,8 @@
 class EmptyEntryError(Exception):
     def __init__(self, msg):
         super().__init__(msg)
+
+
+class CrosswordGenerationError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
