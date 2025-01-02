89d8721321a1bd2a0a909d7ea7489b9b68d53702
root <root@DESKTOP-EJ8HNA8.>
2025-01-02 18:37:55 +0100

    Prototype displaying crosswords

--- crossword.py
+++ crossword.py
@@ -13,3 +13,17 @@ class Crossword:
     @property
     def solution_column(self) -> int:
         return self.__solution_column
+
+    def display_str(self) -> str:
+        res = ""
+        for entry in self.__entries:
+            res += " " * entry[0] + f'{entry[1].word}\n'
+        return res
+
+    def display_unsolved(self) -> str:
+        res = ""
+        definitions = ""
+        for i, entry in enumerate(self.__entries):
+            res += f'{i+1}.' + " " * entry[0] + f'{"_" * len(entry[1].word)}\n'
+            definitions += f'{i+1}. {entry[1].definition}\n'
+        return res + definitions
