b69645ee2eebc742c6fe29f97950e377ce539129
root <root@DESKTOP-EJ8HNA8.>
2025-01-04 10:42:05 +0100

    hotfix: Add exception type for Wordlist clas

--- util.py
+++ util.py
@@ -8,6 +8,11 @@ class EmptyWordlistNameError(Exception):
         super().__init__(msg)
 
 
+class ConflictingEntryNameError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
 class CrosswordGenerationError(Exception):
     def __init__(self, msg):
         super().__init__(msg)
