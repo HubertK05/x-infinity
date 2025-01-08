bb78bb08538ae3cacc01af4bc458cc89947a593e
root <root@DESKTOP-EJ8HNA8.>
2025-01-08 14:33:18 +0100

    Redesign letter so it matches the project

--- letter.py
+++ letter.py
@@ -1,12 +1,16 @@
+from typing import Self
 from util import InvalidLetterError
 
 
 class Letter:
     def __init__(self, letter: str, fixed: bool):
-        if len(letter) != 1:
+        if len(letter) > 1:
             raise InvalidLetterError(f"'{letter}' is not a single character")
         self.__letter = letter
         self.fixed = fixed
+
+    def __eq__(self, other: Self):
+        return self.letter == other.letter and self.fixed == other.fixed
 
     @property
     def letter(self):
@@ -14,6 +18,6 @@ class Letter:
 
     @letter.setter
     def letter(self, letter: str):
-        if len(letter) != 1:
+        if len(letter) > 1:
             raise InvalidLetterError(f"'{letter}' is not a single character")
         self.__letter = letter
--- test_letter.py
+++ test_letter.py
@@ -10,10 +10,17 @@ def test_letter_init():
 
 
 def test_letter_init_fails_with_empty_letter():
-    with pytest.raises(InvalidLetterError):
-        Letter("", False)
+    letter = Letter("", False)
+    assert letter.letter == ""
+    assert not letter.fixed
 
 
 def test_letter_init_fails_with_invalid_length():
     with pytest.raises(InvalidLetterError):
         Letter("aa", False)
+
+
+def test_letter_mutation_fails_with_invalid_length():
+    letter = Letter("a", False)
+    with pytest.raises(InvalidLetterError):
+        letter.letter = "aa"
