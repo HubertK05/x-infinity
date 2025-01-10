a897b73beffdc68694b0d454279734bc44d40332
root <root@DESKTOP-EJ8HNA8.>
2025-01-10 13:44:24 +0100

    Check crosswords from ui

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ class CrosswordWindow(widgets.QMainWindow):
 
         self.__selected_wordlist: Optional[Wordlist] = None
         self.__crossword = None
+        self.__checking = False
+
         self.ui.generate_button.clicked.connect(self.__generate_crossword)
-        self.ui.pushButton.clicked.connect(self.__on_select_wordlist)
+        self.ui.select_wordlist_button.clicked.connect(self.__on_select_wordlist)
         self.ui.crossword.cellChanged.connect(self.__on_cell_changed)
         self.ui.crossword.cellDoubleClicked.connect(self.__on_double_clicked)
+        self.ui.check_answer_button.clicked.connect(self.__on_check_answer)
 
     @property
     def selected_wordlist(self):
@@ -61,6 +64,9 @@ class CrosswordWindow(widgets.QMainWindow):
                 self.ui.crossword.setCurrentCell(row + 1, entries[row + 1][0])
 
     def __on_double_clicked(self, row: int, col: int):
+        if self.__checking:
+            return
+
         entries = self.__crossword.entries
         minimum = entries[row][0]
         maximum = minimum + len(entries[row][1].word) - 1
@@ -71,6 +77,26 @@ class CrosswordWindow(widgets.QMainWindow):
             self.ui.crossword.item(row, col).setText(letter.letter)
             self.ui.crossword.item(row, col).setBackground(QBrush(QColor(224, 255, 224)))
 
+    def __on_check_answer(self):
+        if self.__checking:
+            self.__uncheck()
+        else:
+            self.__check()
+
+    def __check(self):
+        self.__checking = True
+        self.ui.crossword.setEditTriggers(widgets.QAbstractItemView.NoEditTriggers)
+        for row, col, correct in self.__crossword.check():
+            if correct:
+                self.ui.crossword.item(row, col).setBackground(QBrush(QColor(224, 255, 224)))
+            else:
+                self.ui.crossword.item(row, col).setBackground(QBrush(QColor(255, 224, 224)))
+
+    def __uncheck(self):
+        self.__checking = False
+        self.ui.crossword.setEditTriggers(widgets.QAbstractItemView.AnyKeyPressed)
+        self.__repaint_crossword()
+
     def __generate_crossword(self):
         if not self.__selected_wordlist:
             widgets.QMessageBox.critical(self, "Error", "No wordlist selected")
@@ -80,28 +106,39 @@ class CrosswordWindow(widgets.QMainWindow):
             gen = CrosswordGenerator()
             gen.entries = self.__selected_wordlist.entries
             self.__crossword = gen.generate(self.ui.solution_input.text())
-            self.__update_crossword_ui()
+            self.__refresh_crossword()
             self.__update_definition_ui()
         except CrosswordGenerationError as e:
             widgets.QMessageBox.critical(self, "Error", str(e))
             return
 
-    def __update_crossword_ui(self):
+    def __refresh_crossword(self):
+        self.__uncheck()
+        self.ui.crossword_pages.setCurrentIndex(1)
         entries: List[Tuple[int, Entry]] = self.__crossword.entries
         width = self.__crossword.width()
         self.ui.crossword.clear()
         self.ui.crossword.setRowCount(len(entries))
         self.ui.crossword.setColumnCount(width)
+        self.__repaint_crossword()
 
-        for row, (offset, entry) in enumerate(entries):
+    def __repaint_crossword(self):
+        for row, (offset, entry) in enumerate(self.__crossword.entries):
             minimum, maximum = offset, offset + len(entry.word) - 1
-            for col in range(0, width):
-                item = widgets.QTableWidgetItem("")
+            for col in range(0, self.__crossword.width()):
+                item = self.ui.crossword.item(row, col)
+                if not item:
+                    self.ui.crossword.setItem(row, col, widgets.QTableWidgetItem(""))
+                    item = self.ui.crossword.item(row, col)
                 if minimum <= col <= maximum:
-                    item.setBackground(QBrush(QColor(224, 224, 224)))
-                if col == self.__crossword.solution_column:
-                    item.setBackground(QBrush(QColor(192, 192, 192)))
-                self.ui.crossword.setItem(row, col, item)
+                    if self.__crossword.get_letter(row, col - minimum).fixed:
+                        item.setBackground(QBrush(QColor(224, 255, 224)))
+                    elif col == self.__crossword.solution_column:
+                        item.setBackground(QBrush(QColor(192, 192, 192)))
+                    else:
+                        item.setBackground(QBrush(QColor(224, 224, 224)))
+                else:
+                    item.setBackground(QBrush(QColor(255, 255, 255)))
 
     def __update_definition_ui(self):
         self.ui.definitions.clear()
