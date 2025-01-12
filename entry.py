787290c09912592dcbee99b3e5e621389b4d4547
root <root@DESKTOP-EJ8HNA8.>
2025-01-12 09:24:01 +0100

    Allow empty definition

--- entry.py
+++ entry.py
@@ -5,8 +5,6 @@ class Entry:
     def __init__(self, word: str, definition: str):
         if not word:
             raise EmptyEntryError("Word is empty")
-        if not definition:
-            raise EmptyEntryError("Definition is empty")
         self.word = word
         self.definition = definition
 
--- test_entry.py
+++ test_entry.py
@@ -14,6 +14,6 @@ def test_init_entry_empty_word_fails():
         Entry("", "definition")
 
 
-def test_init_entry_empty_definition_fails():
-    with pytest.raises(EmptyEntryError):
-        Entry("word", "")
+def test_init_entry_empty_definition_succeeds():
+    entry = Entry("word", "")
+    assert entry.definition == ""
